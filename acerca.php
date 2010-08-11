fc60def0b5b1720c38e5d61d4c614e80a3c7092d
renich <renich@localhost.(none)>
2010-08-10 20:55:50 -0500

    version inicial del archivo
    cree el archivo para dar un ejemplo de como agregarlo a git y, también,
    para enseñarles a hacer un pull de mi repositorio de github.com

---
+++ acerca.php
@@ -0,0 +1,12 @@
+<?php require_once( $_SERVER{'DOCUMENT_ROOT'} . '/lib/header.inc.php' ); ?>
+<?php require_once( $_SERVER{'DOCUMENT_ROOT'} . '/lib/header.inc.php' ); ?>
+
+        <div id="general">
+            <h1>Acerca</h1>
+
+            <p>
+                Este es el código inicial.
+            </p>
+        </div>
+
+<?php require_once( $_SERVER{'DOCUMENT_ROOT'} . '/lib/footer.inc.php' ); ?>
