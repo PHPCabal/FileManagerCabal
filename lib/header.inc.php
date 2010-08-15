e117d044ad450c8b3982c7e450b02578d16fb0c7
Renich Bon Ciric <renich@ania-pc.casa>
2010-08-15 13:31:08 -0500

    Header standard XHTML1 Strict y un archivo rss
    Agregué un header standard y un archivo rss vacío. El header contiene la mayoría de los elementos necesarios para un sitio web.

--- lib/header.inc.php
+++ lib/header.inc.php
@@ -1,6 +1,24 @@
-<html>
+<?php echo '<?xml version="1.0" encoding="UTF-8"?>' . "\n"; ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es" lang="es">
     <head>
-        <title>FileManager Cabal</title>
+        <!--Meta tags -->
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <meta http-equiv="Content-Language" content="es-MX" />
+        <meta name="keywords" content="FileManagerCabal, FMCabal, File Manager, File, Manager, Cabal, LinuxCabal, Linux Cabal, Linux, GNU & Linux, GNU/Linux, GNU" />
+        <meta name="description" content="Proyecto de aprendizaje en PHP" />
+
+        <!--Title -->
+        <title>FileManagerCabal</title>
+
+        <!--CSS, feeds & favicon -->
+        <link href="/images/favicon.ico" rel="shortcut icon" />
+        <link href="/images/favicon.ico" rel="favicon" />
+        <link href="/css/default.css" media="screen" rel="stylesheet" type="text/css" />
+        <link href="/feed/noticias.rss" rel="alternate" type="application/rss+xml" title="Noticias" />
+
+        <!--Javascript -->
+        <script type="text/javascript" charset="utf-8" src="/js/main.js"></script>
     </head>
 
     <body>
