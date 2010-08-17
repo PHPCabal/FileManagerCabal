f640adb28bb8f7d6e69b85487dce0d8de1e77cb9
Renich Bon Ciric <renich@woralelandia.com>
2010-08-17 01:16:00 -0500

    primer draft de navegador

---
+++ lib/nav.inc.php
@@ -0,0 +1,6 @@
+        <div id="navegacion">
+            <ul id="nav">
+                <li><a href="/inicio.php">inicio</a></li>
+                <li><a href="/acerca.php">acerca</a></li>
+            </ul>>
+        </div>
