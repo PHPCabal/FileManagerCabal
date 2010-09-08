096ac7cfcb19bc00e3c65f0dbdec517cc4ec7014
renich <renich@f13.linuxcabal.org>
2010-09-07 20:58:20 -0500

    no me acuerdo

--- application/config.inc.php
+++ application/config.inc.php
@@ -4,6 +4,23 @@
 // Configuración general
 //
 
+//
+// Base de Datos (MySQL)
+//
+
+// host
+define( 'FMC_DB_HOST', 'localhost' );
+
+// usuario (no usar root!!)
+define( 'FMC_DB_USER', 'fmcabal' );
+
+// contraseña
+define( 'FMC_DB_PASSWORD', 'f4m7c@a.ba3l' );
+
+// nombre de la base de datos
+define( 'FMC_DB_DATABASE', 'fmcabal' );
+
+
 // incluir configuración automática
 require_once( 'lib/config_system.inc.php' );
 
