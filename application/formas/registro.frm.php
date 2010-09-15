2d6d9aa920c918ba3d6c5097521af991ed21259e
Renich Bon Ciric <renich@woralelandia.com>
2010-09-14 21:57:42 -0500

    Ejemplo y symlink a README
    Se actualizaron los retos y se agregó una forma de registro.

---
+++ application/formas/registro.frm.php
@@ -0,0 +1,9 @@
+<form method="POST" action="<?php $_SERVER['PHP_SELF']; ?>">
+    <label>Usuario:</label>
+    <input type="text" name="usuario" value="" />
+
+    <label>Contraseña:</label>
+    <input type="password" name="password" value="" />
+
+    <input type="submit" name="enviar" value="Enviar" />
+</form>
