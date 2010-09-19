650ecaf78e7386ae0e07e16c241004b97f50c0a3
Renich Bon Ciric <renich@woralelandia.com>
2010-09-19 18:09:41 -0500

    Independencia de configuración de DB
    La DB se configura en un archivo aparte para poder excluirlo en el .gitignore y, así, no comprometer nuestro password al hacer commits.
    
    Es más seguro así, según yo.

--- application/config.inc.php
+++ application/config.inc.php
@@ -4,22 +4,6 @@
 // Configuración general
 //
 
-//
-// Base de Datos (MySQL)
-//
-
-// host
-define( 'FMC_DB_HOST', 'localhost' );
-
-// usuario (no usar root!!)
-define( 'FMC_DB_USER', 'fmcabal' );
-
-// contraseña
-define( 'FMC_DB_PASSWORD', 'f4m7c@a.ba3l' );
-
-// nombre de la base de datos
-define( 'FMC_DB_DATABASE', 'fmcabal' );
-
 
 // incluir configuración automática
 require_once( 'lib/config_system.inc.php' );
