c2795f6f7feda4234dac75fcd5f08eed051c982b
Renich Bon Ciric <renich@woralelandia.com>
2010-10-12 19:32:10 -0700

    configuración de db en blanco

--- application/db.inc.php
+++ application/db.inc.php
@@ -5,15 +5,15 @@
 //
 
 // host
-define( 'FMC_DB_HOST', 'localhost' );
+define( 'FMC_DB_HOST', '' );
 
 // usuario (no usar root!!)
-define( 'FMC_DB_USER', 'fmcabal' );
+define( 'FMC_DB_USER', '' );
 
 // contraseña
-define( 'FMC_DB_PASSWORD', 'password' );
+define( 'FMC_DB_PASSWORD', '' );
 
 // nombre de la base de datos
-define( 'FMC_DB_DATABASE', 'fmcabal' );
+define( 'FMC_DB_DATABASE', '' );
 
 ?>
