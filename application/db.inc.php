001551f53379a0391bc3f4e96af534bd081170cc
Renich Bon Ciric <renich@woralelandia.com>
2010-10-12 21:28:09 -0500

    nueva configuración para db

---
+++ application/db.inc.php
@@ -0,0 +1,19 @@
+<?php
+
+//
+// Base de Datos (MySQL)
+//
+
+// host
+define( 'FMC_DB_HOST', 'localhost' );
+
+// usuario (no usar root!!)
+define( 'FMC_DB_USER', 'fmcabal' );
+
+// contraseña
+define( 'FMC_DB_PASSWORD', 'password' );
+
+// nombre de la base de datos
+define( 'FMC_DB_DATABASE', 'fmcabal' );
+
+?>
