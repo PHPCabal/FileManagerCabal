7471a760ed7fbe6788167bb16c35785731c218d2
Renich Bon Ciric <renich@woralelandia.com>
2010-11-09 21:12:50 -0600

    Agregamos la columna de rol
    Ésta modificación marca el comienzo de el sistema de roles y permisos.

--- scripts/db.sql
+++ scripts/db.sql
@@ -4,6 +4,7 @@ CREATE TABLE `usuarios` (
   `usuario` varchar(15) COLLATE utf8_bin NOT NULL,
   `password` varchar(40) COLLATE utf8_bin NOT NULL,
   `email` varchar(400) COLLATE utf8_bin NOT NULL,
+  `rol` SET( 'Administrador', 'Usuario', 'Anónimo' ) DEFAULT 'Anónimo', 
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
---
+++ scripts/ejemplo.sql
@@ -0,0 +1,3 @@
+INSERT INTO `usuarios` VALUES( null, 'Rénich', sha1('password'), 'renich@woralelandia.com', 'Usuario');
+INSERT INTO `usuarios` VALUES( null, 'Jacobo', sha1('holamundo'), 'jacobo@hotmail.com', 'Administrador');
+INSERT INTO `usuarios` VALUES( null, 'beto', sha1('123456'), 'betology@gmail.com', 'Usuario');
