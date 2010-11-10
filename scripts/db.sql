388028a24da7fb61b6add7e14b44f0358322b34c
Renich Bon Ciric <renich@woralelandia.com>
2010-11-09 20:18:30 -0600

    corrección de tamaños de columna
    Se corrigió el password y el email para que pudiesen contener los datos necesarios... bueno, más o menos...
    
    El email lo dejamos en 400 caractéres... ni pedo
    El password en 40 para que le quepa el SHA1

--- scripts/db.sql
+++ scripts/db.sql
@@ -25,8 +25,8 @@ DROP TABLE IF EXISTS `usuarios`;
 CREATE TABLE `usuarios` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `usuario` varchar(15) COLLATE utf8_bin NOT NULL,
-  `password` varchar(35) COLLATE utf8_bin NOT NULL,
-  `email` varchar(255) COLLATE utf8_bin NOT NULL,
+  `password` varchar(40) COLLATE utf8_bin NOT NULL,
+  `email` varchar(400) COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
