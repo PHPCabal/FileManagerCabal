2266fc662aad78625bf423519a47e919361ca6fd
Renich Bon Ciric <renich@woralelandia.com>
2010-11-09 20:05:29 -0600

    Cambio a usar SHA1 y datos de ejemplo
    Cambiamos de MD5 a SHA1 para los hashes del password.
    Agregué un script que agrega datos de ejemplo a la DB.
    
    Gracias por su atención.

---
+++ scripts/datos_de_ejemplo.sql
@@ -0,0 +1,3 @@
+INSERT INTO `usuarios` VALUES( null, 'Rénich', sha1('password'), 'renich@woralelandia.com');
+INSERT INTO `usuarios` VALUES( null, 'Jacobo', sha1('holamundo'), 'jacobo@hotmail.com');
+INSERT INTO `usuarios` VALUES( null, 'beto', sha1('123456'), 'betology@gmail.com');
---
+++ scripts/db.sql
@@ -0,0 +1,44 @@
+-- MySQL dump 10.13  Distrib 5.1.50, for redhat-linux-gnu (i386)
+--
+-- Host: localhost    Database: fmcabal
+-- ------------------------------------------------------
+-- Server version	5.1.50
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `usuarios` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `usuario` varchar(15) COLLATE utf8_bin NOT NULL,
+  `password` varchar(35) COLLATE utf8_bin NOT NULL,
+  `email` varchar(255) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2010-10-19 20:00:56
