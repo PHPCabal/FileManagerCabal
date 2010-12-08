5128704a289f0018eb544c2b2476a47fcffd9336
Renich Bon Ciric <renich@woralelandia.com>
2010-12-07 20:18:39 -0600

    Se agregaron DIRECTORY_SEPARATORS en vez de /

--- application/lib/config_system.inc.php
+++ application/lib/config_system.inc.php
@@ -7,12 +7,12 @@
 // PATH
 
 // path de la aplicación
-define( 'FMC_APP_PATH', realpath( __DIR__ . '/..' ) );
+define( 'FMC_APP_PATH', realpath( __DIR__ . DIRECTORY_SEPARATOR . '..' ) );
 
 // path de url
 
 // definir url parcial
-$url = substr( realpath( FMC_APP_PATH . '/../public' ), strlen( $_SERVER[ 'DOCUMENT_ROOT' ] ) );
+$url = substr( realpath( FMC_APP_PATH . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'public' ), strlen( $_SERVER[ 'DOCUMENT_ROOT' ] ) );
 
 // evitar el //
 if ( '/' == $url ) {
@@ -23,13 +23,11 @@ if ( '/' == $url ) {
 define( 'FMC_URL', $url );
 
 // path de la librería
-define( 'FMC_LIB_PATH', FMC_APP_PATH . '/lib');
+define( 'FMC_LIB_PATH', FMC_APP_PATH . DIRECTORY_SEPARATOR . 'lib' );
 
 // path de las formas
-define( 'FMC_FORM_PATH', FMC_APP_PATH . '/formas');
+define( 'FMC_FORM_PATH', FMC_APP_PATH . DIRECTORY_SEPARATOR . 'formas' );
 
 // Sessiones
-session_save_path( FMC_APP_PATH . '/sessions' );
+session_save_path( FMC_APP_PATH . DIRECTORY_SEPARATOR . 'sessions' );
 session_cache_expire( 86400 / 4 );
-
-?>
