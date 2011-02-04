973aa72916fb004065892f0c53046f68610fa3fb
Carlos A. Gonzalez <betology@gmail.com>
2011-02-04 13:12:52 -0600

    El scripto usuario.sql con los mismos datos que db.inc.php.ejemplo, para agilizar la instalacion

--- application/scripts/usuario.sql
+++ application/scripts/usuario.sql
@@ -1,4 +1,4 @@
 set names utf8;
 use `fmcabal`;
 
-GRANT ALL PRIVILEGES ON `fmcabal`.* TO 'fmcabal'@'localhost' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON `fmcabal`.* TO 'fmcabalUser02'@'localhost' IDENTIFIED BY 'P4s5w@r6';
